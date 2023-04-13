6d1ff699aadde62a1740cee68abb1dbe3f75e7ec
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-13 18:29:06 +0200

    Add configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,11 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
   extends: ['upleveled'],
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+    },
+  ],
 };
 
 module.exports = config;
