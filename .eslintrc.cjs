56425cb7aed9c377bea26b4b3843cabb5c10af88
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-04-17 14:17:32 +0200

    Update config based on new eslint-config updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,6 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
   extends: ['upleveled'],
-  overrides: [
-    {
-      files: ['**/*.cjs'],
-      extends: ['eslint-config-upleveled'],
-    },
-  ],
 };
 
 module.exports = config;
