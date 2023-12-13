b94e9614a25c2f504d9cf5916daa81f8112d53c5
Lukas Prochazka <prochazka.lu@gmail.com>
2023-12-13 12:47:53 +0100

    Fix Stylelint test assertion

--- __tests__/stylelint.test.ts
+++ __tests__/stylelint.test.ts
@@ -2,9 +2,10 @@ import { $ } from 'execa';
 import { expect, test } from 'vitest';
 
 test('Errors correctly reported with Stylelint config', async () => {
-  const { stdout, stderr } = await $({
+  const { exitCode, stdout, stderr } = await $({
     reject: false,
   })`pnpm stylelint '**/*.{css,scss,less,js,tsx}'`;
+  expect(exitCode).toBe(2);
   expect(stdout).toMatchSnapshot();
   expect(stderr).toMatchSnapshot();
 });
