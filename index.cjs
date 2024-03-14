f0701c0291c0759a12b85beb404fe449e1609bc8
Karl Horky <karl.horky@gmail.com>
2024-03-14 23:17:41 +0100

    Ignore @theme for Tailwind CSS v4

--- index.cjs
+++ index.cjs
@@ -14,7 +14,7 @@ const config = {
         'scss/at-rule-no-unknown': [
           true,
           {
-            ignoreAtRules: ['tailwind'],
+            ignoreAtRules: ['tailwind', 'theme'],
           },
         ],
       },
