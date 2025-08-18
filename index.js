8ef462a46c5f77097926b8466321cb2dc47d3598
Karl Horky <karl.horky@gmail.com>
2025-08-18 18:51:07 +0200

    Improve Tailwind CSS rules from stylelint-config-tailwindcss

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /** @type {import('stylelint').Config} */
 const config = {
-  extends: ['stylelint-config-recommended'],
+  extends: [
+    'stylelint-config-recommended',
+    // TODO: Configure ignoreAtRules in
+    // nesting-selector-no-missing-scoping-root rule once it
+    // exists:
+    // - https://github.com/stylelint/stylelint/issues/8728
+    // - https://github.com/stylelint/stylelint/issues/8406#issuecomment-3185835523
+    //
+    // For now, it can be disabled manually in CSS like this:
+    //
+    // ```
+    // /* stylelint-disable nesting-selector-no-missing-scoping-root -- Modify stylelint-config-upleveled to configure ignoreAtRules in rule once it exists https://github.com/stylelint/stylelint/issues/8728 https://github.com/stylelint/stylelint/issues/8406#issuecomment-3185835523 */
+    // ```
+  ],
   rules: {
     'no-descending-specificity': null,
     // Allow files without any styles
@@ -8,7 +21,13 @@ const config = {
   },
   overrides: [
     {
-      files: ['**/*.css', '**/*.scss', '**/*.sass', '**/*.less'],
+      files: [
+        '**/*.scss',
+        '**/*.sass',
+        '**/*.less',
+        // Tailwind CSS with PostCSS features
+        '**/app/globals.css',
+      ],
       extends: ['stylelint-config-recommended-scss'],
       rules: {
         'at-rule-no-deprecated': [
@@ -21,17 +40,35 @@ const config = {
           true,
           {
             ignoreAtRules: [
-              // Tailwind CSS Directives
-              // - https://tailwindcss.com/docs/functions-and-directives
-              'config',
+              // Configuration taken from
+              // stylelint-config-tailwindcss
+              // - https://github.com/zhilidali/stylelint-config-tailwindcss/blob/188cf5c210c23d3f7be285d8cae4a7c1620fdefb/index.js#L4-L26
+              //
+              // Tailwind CSS v4
+              'custom-variant',
               'plugin',
               'source',
-              'tailwind',
               'theme',
               'utility',
               'variant',
-              'custom-variant',
+              // Tailwind CSS v3
+              'apply',
+              'config',
+              'layer',
+              'tailwind',
+              // Tailwind CSS v1, v2
+              'responsive',
+              'screen',
+              'variants',
             ],
+          },
+        ],
+        // Configuration taken from stylelint-config-tailwindcss
+        // - https://github.com/zhilidali/stylelint-config-tailwindcss/blob/188cf5c210c23d3f7be285d8cae4a7c1620fdefb/index.js#L27-L32
+        'scss/function-no-unknown': [
+          true,
+          {
+            ignoreFunctions: ['theme'],
           },
         ],
       },
